34a8026ae30a800939673c93d2d81d0107303057
Javidp <dawid.cpp@gmail.com>
2016-08-27 01:08:09 +0200

    #15 Move Mobile Applications to left menu under Rewards menu bar

--- afm-ear-web/src/main/java/is/web/beans/tab/SentinelTabs.java
+++ afm-ear-web/src/main/java/is/web/beans/tab/SentinelTabs.java
@@ -3,159 +3,159 @@ package is.web.beans.tab;
 public class SentinelTabs {
 
 	//setupRewardTypes.xhtml
-	public static String TAB_SUPPORTED_REWARD_TYPES = "tabSupportedRewardTypes";
+	public static final String TAB_SUPPORTED_REWARD_TYPES = "tabSupportedRewardTypes";
 
 	//customDenominationModels.xhtml
-	public static String TAB_CUSTOM_DENOMINATION_MODELS = "tabCustomDenominationModels";
+	public static final String TAB_CUSTOM_DENOMINATION_MODELS = "tabCustomDenominationModels";
 
 	//generatedOffers.xhtml
-	public static String TAB_DISPLAY_GENERATED_OFFERS = "tabGeneratedOffers";
+	public static final String TAB_DISPLAY_GENERATED_OFFERS = "tabGeneratedOffers";
 
 	//blockedOffers.xhtml
-	public static String TAB_DISPLAY_APP_USERS = "tabDisplayAppUsers";
+	public static final String TAB_DISPLAY_APP_USERS = "tabDisplayAppUsers";
 		
 	//blockedOffers.xhtml
-	public static String TAB_BLOCKED_OFFERS = "tabBlockedOffers";
+	public static final String TAB_BLOCKED_OFFERS = "tabBlockedOffers";
 
 	//setupAdProviders.xhtml
-	public static String TAB_AD_PROVIDERS = "tabAdProviders";
+	public static final String TAB_AD_PROVIDERS = "tabAdProviders";
 
 	//setupDenominationModels.xhtml
-	public static String TAB_DENOMINATION_MODELS = "tabDenominationModels";
+	public static final String TAB_DENOMINATION_MODELS = "tabDenominationModels";
 
 	//setupOfferWalls.xhtml
-	public static String TAB_OFFER_WALLS = "tabOfferWalls";
+	public static final String TAB_OFFER_WALLS = "tabOfferWalls";
 
 	//setupCurrencies.xhtml
-	public static String TAB_SETUP_CURRENCIES = "tabSupportedCurrencies";
+	public static final String TAB_SETUP_CURRENCIES = "tabSupportedCurrencies";
 	
 	//setupCountries.xhtml
-	public static String TAB_COUNTRIES = "tabCountries";
+	public static final String TAB_COUNTRIES = "tabCountries";
 
 	//setupOfferFilters.xhtml
-	public static String TAB_SETUP_OFFER_FILTERS = "tabOfferFilters";
+	public static final String TAB_SETUP_OFFER_FILTERS = "tabOfferFilters";
 
 	//systemSettings.xhtml
-	public static String TAB_SYSTEM_SETTINGS = "tabSystemSettings";
+	public static final String TAB_SYSTEM_SETTINGS = "tabSystemSettings";
 
 	//manageUsers.xhtml
-	public static String TAB_MANAGE_USERS = "tabManageUsers";
+	public static final String TAB_MANAGE_USERS = "tabManageUsers";
 
 	//licenseManagement.xhtml
-	public static String TAB_LICENSE_MANAGEMENT = "tabLicenseManagement";
+	public static final String TAB_LICENSE_MANAGEMENT = "tabLicenseManagement";
 
 	//api.xhtml
-	public static String TAB_NETWORK_SETTINGS = "tabNetworkSettings";
+	public static final String TAB_NETWORK_SETTINGS = "tabNetworkSettings";
+	
+	//mobileApplications.xhtml
+	public static final String TAB_MOBILE_APPLICATIONS = "tabMobileApplications";
 	
 	//quidcoEventBrowser.xhtml
-	public static String TAB_QUIDCO_EVENT_BROWSER = "tabQuidcoEventBrowser";
+	public static final String TAB_QUIDCO_EVENT_BROWSER = "tabQuidcoEventBrowser";
 
 	//snapdealEventBrowser.xhtml
-	public static String TAB_SNAPDEAL_EVENT_BROWSER = "tabSnapdealEventBrowser";
+	public static final String TAB_SNAPDEAL_EVENT_BROWSER = "tabSnapdealEventBrowser";
 	
 	//eventBrowser.xhtml
-	public static String TAB_EVENT_BROWSER = "tabEventBrowser";
+	public static final String TAB_EVENT_BROWSER = "tabEventBrowser";
 	
 	//rewardTickets.xhtml
-	public static String TAB_REWARD_TICKETS = "tabRewardTickets";
+	public static final String TAB_REWARD_TICKETS = "tabRewardTickets";
 	
 	//walletTransactions.xhtml
-	public static String TAB_WALLET_TRANSACTION = "tabWalletTransactions"; 
+	public static final String TAB_WALLET_TRANSACTION = "tabWalletTransactions"; 
 
 	//systemStatus.xhtml
-	public static String TAB_SYSTEM_STATUS = "tabSystemStatus";
+	public static final String TAB_SYSTEM_STATUS = "tabSystemStatus";
 
 	//conversionStats.xhtml
-	public static String TAB_CONVERSION_STATS = "tabConversionStats";
+	public static final String TAB_CONVERSION_STATS = "tabConversionStats";
 
 	//referrals.xhtml
-	public static String TAB_REFERRALS = "tabReferrals";
+	public static final String TAB_REFERRALS = "tabReferrals";
 	
 	
 	//rewardUser.xhtml
-	public static String TAB_SYSTEM_WALLET_SETTINGS = "tabWalletSettings";
+	public static final String TAB_SYSTEM_WALLET_SETTINGS = "tabWalletSettings";
 	
-	public static String TAB_PROVIDER_REPORT = "tabProviderReport";
+	public static final String TAB_PROVIDER_REPORT = "tabProviderReport";
 	
-	public static String TAB_APPLICATIONS_REWARD = "tabApplicationsReward";
+	public static final String TAB_APPLICATIONS_REWARD = "tabApplicationsReward";
 	
-	public static String TAB_VIDEO_GAMIFICATIONS_SETTINGS = "tabVideoGamificationSettings";
+	public static final String TAB_VIDEO_GAMIFICATIONS_SETTINGS = "tabVideoGamificationSettings";
 	
-	public static String TAB_SPINNER_GAMIFICATIONS_SETTINGS = "tabSpinnerGamificationSettings";
-	
-	//rewardUser.xhtml
-	public static String TAB_TOOLS_REWARD_USERS = "tabRewardUser";
+	public static final String TAB_SPINNER_GAMIFICATIONS_SETTINGS = "tabSpinnerGamificationSettings";
 	
 	//rewardUser.xhtml
-	public static String TAB_TOOLS_REWARD_SPIN = "tabRewardSpin";
+	public static final String TAB_TOOLS_REWARD_USERS = "tabRewardUser";
+	
+	//rewardUser.xhtml
+	public static final String TAB_TOOLS_REWARD_SPIN = "tabRewardSpin";
 	
 	//failedEventsBrowser.xhtml
-	public static String TAB_FAILED_EVENTS_BROWSER = "tabFailedEventsBrowser";
+	public static final String TAB_FAILED_EVENTS_BROWSER = "tabFailedEventsBrowser";
 	
 	//spinnerStats.xhtml
-	public static String TAB_SPINNER_STATS = "tabSpinnerStats";
+	public static final String TAB_SPINNER_STATS = "tabSpinnerStats";
 	
 
 	//-------------------------------------- domain config -------------------------------------
 	//autoregister.xhtml
-	public static String TAB_SENTINEL_AUTOREGISTER = "tabAutoregister";
+	public static final String TAB_SENTINEL_AUTOREGISTER = "tabAutoregister";
 
 	//devices.xhtml
-	public static String TAB_SENTINEL_DEVICES_LIST = "tabSentinelDevicesList";
+	public static final String TAB_SENTINEL_DEVICES_LIST = "tabSentinelDevicesList";
 
 	//devicesStatus.xhtml
-	public static String TAB_SENTINEL_DEVICES_STATUS = "tabDevicesStatus";
+	public static final String TAB_SENTINEL_DEVICES_STATUS = "tabDevicesStatus";
 
 	//findDevice.xhtml
-	public static String TAB_SENTINEL_FIND_DEVICE = "tabSentinelFindDevice";
+	public static final String TAB_SENTINEL_FIND_DEVICE = "tabSentinelFindDevice";
 
 	//assignDevicesToDomains.xhtml
-	public static String TAB_SENTINEL_DOMAINS_ASSIGN_DEVICES_TO_DOMAINS = "tabSentinelDomainsAssignDevicesToDomains";
+	public static final String TAB_SENTINEL_DOMAINS_ASSIGN_DEVICES_TO_DOMAINS = "tabSentinelDomainsAssignDevicesToDomains";
 	
 	//------------------------------------- cwmp config ----------------------------------------
 	//cwmpConfigureMonitoringScripts.xhtml
-	public static String TAB_SENTINEL_CWMP_CONFIGURE_MONITORING_SCRIPTS = "tabSentinelCWMPConfigureMonitoringScripts";
+	public static final String TAB_SENTINEL_CWMP_CONFIGURE_MONITORING_SCRIPTS = "tabSentinelCWMPConfigureMonitoringScripts";
 
 	//cwmpConfigureDeviceProfiles.xhtml
-	public static String TAB_SENTINEL_CWMP_CONFIGURE_DEVICE_PROFILES = "tabSentinelCWMPConfigureDeviceProfiles";
+	public static final String TAB_SENTINEL_CWMP_CONFIGURE_DEVICE_PROFILES = "tabSentinelCWMPConfigureDeviceProfiles";
 
 	//cwmpConfigureHardwareModels.xhtml
-	public static String TAB_SENTINEL_CWMP_CONFIGURE_HARDWARE_MODELS = "tabSentinelCWMPConfigureHardwareModels";
+	public static final String TAB_SENTINEL_CWMP_CONFIGURE_HARDWARE_MODELS = "tabSentinelCWMPConfigureHardwareModels";
 	
 	//cwmpConfigureAutoConfigurationScripts.xhtml
-	public static String TAB_SENTINEL_CWMP_CONFIGURE_AUTOCONFIGURATION_SCRIPTS = "tabSentinelCWMPConfigureAutoConfigurationScripts";
+	public static final String TAB_SENTINEL_CWMP_CONFIGURE_AUTOCONFIGURATION_SCRIPTS = "tabSentinelCWMPConfigureAutoConfigurationScripts";
 
 	//cwmpConfigureProvisioning.xhtml
-	public static String TAB_SENTINEL_CWMP_CONFIGURE_PROVISIONING = "tabSentinelCWMPConfigureProvisioning";
+	public static final String TAB_SENTINEL_CWMP_CONFIGURE_PROVISIONING = "tabSentinelCWMPConfigureProvisioning";
 	
 	//cwmpMassManagementProfiling.xhtml
-	public static String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_PROFILING = "tabSentinelCWMPMassManagementProfiling";
+	public static final String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_PROFILING = "tabSentinelCWMPMassManagementProfiling";
 	
 	//cwmpMassManagementMonitoring.xhtml
-	public static String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_MONITORING = "tabSentinelCWMPMassManagementMonitoring";
+	public static final String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_MONITORING = "tabSentinelCWMPMassManagementMonitoring";
 	
 	//cwmpMassManagementAutoconfiguration.xhtml
-	public static String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_AUTOCONFIGURATION = "tabSentinelCWMPMassManagementAutoconfiguration";
+	public static final String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_AUTOCONFIGURATION = "tabSentinelCWMPMassManagementAutoconfiguration";
 	
 	//cwmpMassManagementProvisioning.xhtml
-	public static String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_PROVISIONING = "tabSentinelCWMPMassManagementProvisioning";
+	public static final String TAB_SENTINEL_CWMP_MASS_MANAGEMENT_PROVISIONING = "tabSentinelCWMPMassManagementProvisioning";
 	
 	//devicesMap.xhtml
-	public static String TAB_SENTINEL_MONITORING_DEVICES_MAP = "tabDevicesMap";
+	public static final String TAB_SENTINEL_MONITORING_DEVICES_MAP = "tabDevicesMap";
 
 	//deviceAlerts.xhtml
-	public static String TAB_SENTINEL_MONITORING_SETUP = "tabMonitoringSetup";
+	public static final String TAB_SENTINEL_MONITORING_SETUP = "tabMonitoringSetup";
 
 	//deviceAlerts.xhtml
-	public static String TAB_SENTINEL_MONITORING_DEVICES_ALERTS = "tabDeviceAlerts";
+	public static final String TAB_SENTINEL_MONITORING_DEVICES_ALERTS = "tabDeviceAlerts";
 	
 	//systemAlerts.xhtml
-	public static String TAB_SENTINEL_MONITORING_SYSTEM_ALERTS = "tabSystemAlerts";
+	public static final String TAB_SENTINEL_MONITORING_SYSTEM_ALERTS = "tabSystemAlerts";
 
 	//reportsAlerts.xhtml
-	public static String TAB_REPORTS_ALERTS = "tabReportsAlerts";
+	public static final String TAB_REPORTS_ALERTS = "tabReportsAlerts";
 
-	
-
-	
 }
