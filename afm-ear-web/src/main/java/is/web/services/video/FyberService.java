fa53634c8294a42701b4fa2ab984ec1adf4a75a4
Jakub Homlala <jhomlala@gmail.com>
2016-09-02 12:22:10 +0200

    Fyber testing

--- afm-ear-web/src/main/java/is/web/services/video/FyberService.java
+++ afm-ear-web/src/main/java/is/web/services/video/FyberService.java
@@ -3,12 +3,14 @@ package is.web.services.video;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/")
 public class FyberService {
 
-	@Path("/video/fyber/reward")
+	/*@Path("/video/fyber/reward")
 	@GET
 	public Response rewardCallback(@QueryParam("uid") String uid, @QueryParam("amount") int amount,
 			@QueryParam("currency_id") String currencyId, @QueryParam("currency_name") String currencyName,
@@ -19,5 +21,17 @@ public class FyberService {
 		System.out.println("Currency name: " + currencyName);
 		System.out.println("UserId: " + userId);
 		return Response.accepted().build();
+	}*/
+	
+	@Path("/video/fyber/reward")
+	@GET
+	public Response rewardCallback(@Context UriInfo info) {
+		System.out.println("FYBER CALLBACK");
+		System.out.println(info.toString());
+		System.out.println(info.getPath());
+		System.out.println(info.getAbsolutePath());
+		return Response.accepted().build();
 	}
+	
+	
 }
