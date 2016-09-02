55f9c1a18652c89ecea2dbec0b31bebad45a5743
Jakub Homlala <jhomlala@gmail.com>
2016-09-02 12:15:36 +0200

    Updated Fyber Video Service

--- afm-ear-web/src/main/java/is/web/services/video/FyberService.java
+++ afm-ear-web/src/main/java/is/web/services/video/FyberService.java
@@ -10,12 +10,14 @@ public class FyberService {
 
 	@Path("/video/fyber/reward")
 	@GET
-	public Response rewardCallback(@QueryParam("uid") int uid, @QueryParam("amount") int amount,
-			@QueryParam("currency_id") String currencyId, @QueryParam("currency_name") String currencyName) {
+	public Response rewardCallback(@QueryParam("uid") String uid, @QueryParam("amount") int amount,
+			@QueryParam("currency_id") String currencyId, @QueryParam("currency_name") String currencyName,
+			@QueryParam("userId") String userId) {
 		System.out.println("UID: " + uid);
 		System.out.println("Amount: " + amount);
 		System.out.println("Currency Id: " + currencyId);
 		System.out.println("Currency name: " + currencyName);
+		System.out.println("UserId: " + userId);
 		return Response.accepted().build();
 	}
 }
