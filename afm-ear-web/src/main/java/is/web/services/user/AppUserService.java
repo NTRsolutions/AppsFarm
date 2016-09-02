ab36e1d61bb19c30d8223c4577c0e960f29b6397
Jakub Homlala <jhomlala@gmail.com>
2016-09-02 08:16:53 +0200

    Added User Update endpoint

--- afm-ear-web/src/main/java/is/web/services/user/AppUserService.java
+++ afm-ear-web/src/main/java/is/web/services/user/AppUserService.java
@@ -42,6 +42,8 @@ import is.web.services.user.validators.UsernameDBValidator;
 import is.web.services.user.validators.UsernameValidator;
 import is.web.services.wall.validators.DeviceValidator;
 import is.web.services.wall.validators.PhoneValidator;
+import is.web.services.wall.validators.RequestValidator;
+import is.web.services.wallet.validators.UsernamePasswordCombinationValidator;
 
 @Path("/")
 public class AppUserService {
@@ -75,10 +77,14 @@ public class AppUserService {
 	@Inject
 	private DeviceTokenValidator deviceTokenValidator;
 	@Inject
+	private UsernamePasswordCombinationValidator usernamePasswordCombinationValidator;
+	@Inject
+	private RequestValidator requestValidator;
+	@Inject
 	private APIHelper apiHelper;
 	@Inject
 	private AttendanceManager attendanceManager;
-	
+
 	@Path("/user/register")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -92,7 +98,7 @@ public class AppUserService {
 		validateRegisterRequest(apiRequestDetails, response);
 		if (response.getStatus() == null || !response.getStatus().equals(RespStatusEnum.FAILED)) {
 			AppUserEntity insertedUser = insertUser(apiRequestDetails);
-			if (insertedUser !=  null){
+			if (insertedUser != null) {
 				apiHelper.setupSuccessResponse(response);
 				insertedUser.setPassword("");
 				response.setAppUserEntity(insertedUser);
@@ -173,7 +179,7 @@ public class AppUserService {
 			appUser.setPhoneId((String) parameters.get("phoneId"));
 			appUser.setAndroidDeviceToken((String) parameters.get("androidDeviceToken"));
 			appUser.setAttendanceLastBonusTime(new Timestamp(new Date().getTime()));
-			
+
 			if (parameters.containsKey("firstName")) {
 				appUser.setFirstName((String) parameters.get("firstName"));
 			}
@@ -209,6 +215,7 @@ public class AppUserService {
 		validators.add(phoneValidator);
 		validators.add(deviceValidator);
 		validators.add(deviceTokenValidator);
+		validators.add(requestValidator);
 		return validators;
 	}
 
@@ -275,6 +282,7 @@ public class AppUserService {
 		validators.add(deviceTokenValidator);
 		validators.add(deviceValidator);
 		validators.add(phoneValidator);
+		validators.add(requestValidator);
 		return validators;
 	}
 
@@ -293,7 +301,6 @@ public class AppUserService {
 		String username = (String) parameters.get("username");
 		String password = (String) parameters.get("password");
 
-		
 		AppUserEntity appUser = getUser(username);
 		if (appUser == null) {
 			Application.getElasticSearchLogger().indexLog(Application.USER_LOGIN_ACTIVITY, -1, LogStatus.ERROR,
@@ -302,7 +309,7 @@ public class AppUserService {
 		} else {
 			String hashedPassword = getPasswordHash(password);
 			if (appUser.getPassword().equals(hashedPassword)) {
-				appUser = updateAppUserDetailsIfNecessary(appUser,apiRequestDetails);
+				appUser = updateAppUserDetailsIfNecessary(appUser, apiRequestDetails);
 				appUser.setPassword("");
 				apiHelper.setupSuccessResponse(response);
 				response.setAppUserEntity(appUser);
@@ -312,24 +319,23 @@ public class AppUserService {
 			}
 		}
 	}
-	
-	
-	private AppUserEntity updateAppUserDetailsIfNecessary(AppUserEntity appUser,APIRequestDetails apiRequestDetails){
+
+	private AppUserEntity updateAppUserDetailsIfNecessary(AppUserEntity appUser, APIRequestDetails apiRequestDetails) {
 		boolean isUpdate = false;
-		HashMap<String,Object> parameters = apiRequestDetails.getParameters();
+		HashMap<String, Object> parameters = apiRequestDetails.getParameters();
 		String advertisingId = (String) parameters.get("advertisingId");
 		String androidDeviceToken = (String) parameters.get("androidDeviceToken");
 		String deviceId = (String) parameters.get("deviceId");
 		String phoneId = (String) parameters.get("phoneId");
 		if (!appUser.getAdvertisingId().equals(advertisingId)) {
 			Application.getElasticSearchLogger().indexLog(Application.USER_LOGIN_ACTIVITY, -1, LogStatus.OK,
-					Application.USER_LOGIN_ACTIVITY + " updating advertisingId for request : "
-							+ apiRequestDetails + " for appuser: " + appUser);
+					Application.USER_LOGIN_ACTIVITY + " updating advertisingId for request : " + apiRequestDetails
+							+ " for appuser: " + appUser);
 			appUser.setAdvertisingId(advertisingId);
 			logger.info("Updated advertisingId for appUser: " + appUser);
 			isUpdate = true;
 		}
-		if (!appUser.getAndroidDeviceToken().equals(androidDeviceToken)){
+		if (!appUser.getAndroidDeviceToken().equals(androidDeviceToken)) {
 			Application.getElasticSearchLogger().indexLog(Application.USER_LOGIN_ACTIVITY, -1, LogStatus.OK,
 					Application.USER_LOGIN_ACTIVITY + " updating android device token for request : "
 							+ apiRequestDetails + " for appuser: " + appUser);
@@ -337,32 +343,107 @@ public class AppUserService {
 			logger.info("Updated android device token for appUser: " + appUser);
 			isUpdate = true;
 		}
-		
-		if (!appUser.getDeviceId().equals(deviceId)){
+
+		if (!appUser.getDeviceId().equals(deviceId)) {
 			Application.getElasticSearchLogger().indexLog(Application.USER_LOGIN_ACTIVITY, -1, LogStatus.OK,
-					Application.USER_LOGIN_ACTIVITY + " updating device id for request : "
-							+ apiRequestDetails + " for appuser: " + appUser);
+					Application.USER_LOGIN_ACTIVITY + " updating device id for request : " + apiRequestDetails
+							+ " for appuser: " + appUser);
 			appUser.setDeviceId(deviceId);
 			logger.info("Updated device id for appUser: " + appUser);
 			isUpdate = true;
 		}
-		
-		if (!appUser.getPhoneId().equals(phoneId)){
+
+		if (!appUser.getPhoneId().equals(phoneId)) {
 			Application.getElasticSearchLogger().indexLog(Application.USER_LOGIN_ACTIVITY, -1, LogStatus.OK,
-					Application.USER_LOGIN_ACTIVITY + " updating phone id for request : "
-							+ apiRequestDetails + " for appuser: " + appUser);
+					Application.USER_LOGIN_ACTIVITY + " updating phone id for request : " + apiRequestDetails
+							+ " for appuser: " + appUser);
 			appUser.setPhoneId(phoneId);
 			logger.info("Updated phone id for appUser: " + appUser);
 			isUpdate = true;
 		}
-		
-		if (isUpdate){
+
+		if (isUpdate) {
 			daoAppUser.createOrUpdate(appUser);
 		}
 		return appUser;
 	}
-	
 
-	
+	@Path("/user/update")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String updateUser(final APIRequestDetails apiRequestDetails) {
+		APIResponse response = new APIResponse();
+		try {
+			logger.info("Received update request:" + apiRequestDetails);
+			Application.getElasticSearchLogger().indexLog(Application.USER_UPDATE_ACTIVITY, -1, LogStatus.OK,
+					Application.USER_UPDATE_ACTIVITY + " received user update request: " + apiRequestDetails);
+			HashMap<String, Object> parameters = apiRequestDetails.getParameters();
+			logger.info("** Validating user update request: **");
+			for (APIValidator validator : getUserUpdateValidators()) {
+				if (!validator.validate(parameters)) {
+					logger.info("-> Validator: " + validator.getClass() + " FAILED");
+					apiHelper.setupFailedResponseForError(response, validator.getInvalidValueErrorCode());
+					Application.getElasticSearchLogger().indexLog(Application.USER_UPDATE_ACTIVITY, -1, LogStatus.ERROR,
+							Application.USER_UPDATE_ACTIVITY + " validation failed for request: " + apiRequestDetails
+									+ " in: " + validator.getClass() + " cause: "
+									+ validator.getInvalidValueErrorCode());
+					return apiHelper.getGson().toJson(response);
+				} else {
+					logger.info("-> Validator: " + validator.getClass() + " OK");
+				}
+			}
+
+			boolean result = updateUser(parameters);
+			if (result){
+				apiHelper.setupSuccessResponse(response);
+			} else {
+				apiHelper.setupFailedResponseForError(response, RespCodesEnum.ERROR_INTERNAL_SERVER_ERROR);
+			}
+			return apiHelper.getGson().toJson(response);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			apiHelper.setupFailedResponseForError(response, RespCodesEnum.ERROR_INTERNAL_SERVER_ERROR);
+			return apiHelper.getGson().toJson(response);
+		}
+	}
+
+	private boolean updateUser(HashMap<String, Object> parameters) {
+		try {
+			boolean isUpdate = false;
+			AppUserEntity appUser = daoAppUser.findByUsername((String) parameters.get("username"));
+			if (parameters.containsKey("androidDeviceToken")) {
+				appUser.setAndroidDeviceToken((String) parameters.get("androidDeviceToken"));
+				isUpdate = true;
+			}
+			if (parameters.containsKey("ageRange")) {
+				appUser.setAgeRange((String) parameters.get("ageRange"));
+				isUpdate = true;
+			}
+			if (parameters.containsKey("gender")) {
+				appUser.setGender((String) parameters.get("gender"));
+				isUpdate = true;
+			}
+			if (isUpdate){
+				daoAppUser.createOrUpdate(appUser);
+				Application.getElasticSearchLogger().indexLog(Application.USER_UPDATE_ACTIVITY, -1, LogStatus.ERROR,
+						Application.USER_UPDATE_ACTIVITY + " Updated appUser: " + appUser);
+				logger.info("Updated appUser: " + appUser);
+			}
+			return true;
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			return false;
+		}
+	}
+
+	private List<APIValidator> getUserUpdateValidators() {
+		List<APIValidator> validators = new ArrayList<APIValidator>();
+		validators.add(usernameValidator);
+		validators.add(passwordValidator);
+		validators.add(usernamePasswordCombinationValidator);
+		validators.add(requestValidator);
+		return validators;
+	}
 
 }
